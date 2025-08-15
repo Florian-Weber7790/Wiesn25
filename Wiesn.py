828eba8781a0f4c8ac6e78065324c916f91f7b08
Florian-Weber7790 <info@weberflorian.de>
2025-08-15 13:00:54 +0200

    Wiesn.py

--- Wiesn.py
+++ Wiesn.py
@@ -43,6 +43,10 @@ MITARBEITER = [
     ).split(",") if m.strip()
 ]
 
+EDIT_START = _get_env_date("EDIT_START", "2025-09-20")
+EDIT_END   = _get_env_date("EDIT_END", "2025-10-05")
+
+# Passwörter aus ENV einlesen
 def _parse_password_map(default_names):
     # Erwartet ENV: MITARBEITER_PASSWORDS="Julia:pw1,Regina:pw2,Florian:pw3"
     raw = os.getenv("MITARBEITER_PASSWORDS", "")
